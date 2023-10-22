8797f0ac335871df844385ad3abda4e23cefb926
PhiVietHoang <phiviethoang14@gmail.com>
2023-10-22 23:07:13 +0700

    Feat: add employee controller and route

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv')
 const connectWithDB = require('./config/mongoose.js');
+const cookieParser = require('cookie-parser');
+const EmployeeRoute = require('./routes/EmployeeRoute');
+const CompanyRoute = require('./routes/CompanyRoute');
 require('dotenv').config();
 
 // connect with database
 connectWithDB();
 
 const app = express()
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
 
-app.get('/',(req, res)=>res.send('Hello'))
+app.use('/employee', EmployeeRoute);
+app.use('/company', CompanyRoute);
 
 const PORT = process.env.PORT;
 
