c724ee5b8dbf937401b73f566aa057084e3f71d6
nguyen-hai-nam <nam.nn265@gmail.com>
2023-10-22 13:50:13 +0700

    Integrate home page into app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import './index.css'
+import App from './App.tsx'
+import Home from './pages/Home.tsx'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            }
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 )
