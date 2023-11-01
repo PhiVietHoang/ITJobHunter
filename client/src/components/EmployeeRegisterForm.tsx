1e0aa5648e5587d3d678a0e3fcf3fb707c70619f
nguyen-hai-nam <nam.nn265@gmail.com>
2023-11-01 15:44:19 +0700

    Feat: create employee auth forms

---
+++ client/src/components/EmployeeRegisterForm.tsx
@@ -0,0 +1,62 @@
+import { Link } from 'react-router-dom'
+
+const EmployeeRegisterForm = () => {
+    return (
+        <form className='px-16 py-12 flex flex-col gap-6 bg-white rounded-lg'>
+            <div className='mx-auto text-3xl font-bold text-cyan-600'>ITJobHunter</div>
+            <div className='flex justify-between items-end'>
+                <span className='text-2xl font-semibold'>Register</span>
+                <span className='text-xl font-semibold text-[#275df5] hover:underline cursor-pointer'>
+                    <Link to='../login'>Already have an account?</Link>
+                </span>
+            </div>
+            <div className='flex flex-col gap-2'>
+                <label htmlFor='email' className='font-semibold'>
+                    Email
+                </label>
+                <input
+                    type='email'
+                    id='email'
+                    name='email'
+                    required
+                    placeholder='Enter your email'
+                    className='py-3 px-4 rounded-2xl border-black border focus:outline-[#275df5]'
+                />
+            </div>
+            <div className='flex flex-col gap-2'>
+                <label htmlFor='password' className='font-semibold'>
+                    Password
+                </label>
+                <input
+                    type='password'
+                    id='password'
+                    name='password'
+                    required
+                    placeholder='Enter your password'
+                    className='py-3 px-4 rounded-2xl border-black border focus:outline-[#275df5]'
+                />
+            </div>
+            <div className='flex flex-col gap-2'>
+                <label htmlFor='password' className='font-semibold'>
+                    Confirm password
+                </label>
+                <input
+                    type='password'
+                    id='passwordConfirm'
+                    name='passwordConfirm'
+                    required
+                    placeholder='Confirm your password'
+                    className='py-3 px-4 rounded-2xl border-black border focus:outline-[#275df5]'
+                />
+            </div>
+            <button
+                type='submit'
+                className='my-6 py-4 w-full text-xl text-semibold text-white bg-[#275df5] hover:bg-[#275df5cc] rounded-[60px]'
+            >
+                Register
+            </button>
+        </form>
+    )
+}
+
+export default EmployeeRegisterForm
