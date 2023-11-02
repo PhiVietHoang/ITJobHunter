ef2fce078ba14602896249e198e1b78a661e4260
nguyen-hai-nam <nam.nn265@gmail.com>
2023-11-02 11:44:15 +0700

    Feat: create api services

---
+++ client/src/services/api.ts
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+const API_URL = import.meta.env.VITE_API_URL
+
+const api = axios.create({
+    baseURL: API_URL,
+    validateStatus: function (status) {
+        return status >= 200 && status < 500 // Accepts responses with status code in the range 200-499
+    }
+})
+
+export const register = async (requestBody: { name: string; email: string; password: string }) => {
+    try {
+        const { name, email, password } = requestBody
+        const response = await api.post('employee/register', { name, email, password })
+        return response
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const login = async (requestBody: { email: string; password: string }) => {
+    try {
+        const { email, password } = requestBody
+        const response = await api.post('employee/login', { email, password })
+        return response
+    } catch (error) {
+        console.error(error)
+    }
+}
