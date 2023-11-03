df79b373087b6fa6600e1149d4225ab1c4c63f31
nguyen-hai-nam <nam.nn265@gmail.com>
2023-11-03 11:19:43 +0700

    Feat: integrate login api

--- client/src/components/EmployeeLoginForm.tsx
+++ client/src/components/EmployeeLoginForm.tsx
@@ -1,8 +1,38 @@
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { login } from '~/services/api'
 
 const EmployeeLoginForm = () => {
+    const navigate = useNavigate()
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+    const [formMessage, setFormMessage] = useState({ type: '', message: '' })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        try {
+            const response = await login(formData)
+            if (response?.status === 200) {
+                navigate('/')
+            } else if (response) {
+                setFormMessage({ type: 'error', message: response.data.message })
+            } else {
+                setFormMessage({ type: 'error', message: 'Something went wrong' })
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
-        <form className='px-16 py-12 flex flex-col gap-6 bg-white rounded-lg'>
+        <form className='px-16 py-12 flex flex-col gap-6 bg-white rounded-lg' onSubmit={onSubmit}>
             <div className='mx-auto text-3xl font-bold text-cyan-600'>ITJobHunter</div>
             <div className='flex justify-between items-end'>
                 <span className='text-2xl font-semibold'>Login</span>
@@ -21,6 +51,8 @@ const EmployeeLoginForm = () => {
                     required
                     placeholder='Enter your email'
                     className='py-3 px-4 rounded-2xl border-black border focus:outline-[#275df5]'
+                    value={formData.email}
+                    onChange={handleChange}
                 />
             </div>
             <div className='flex flex-col gap-2'>
@@ -34,8 +66,19 @@ const EmployeeLoginForm = () => {
                     required
                     placeholder='Enter your password'
                     className='py-3 px-4 rounded-2xl border-black border focus:outline-[#275df5]'
+                    value={formData.password}
+                    onChange={handleChange}
                 />
             </div>
+            {formMessage && (
+                <p
+                    className={`${
+                        formMessage.type === 'error' ? 'text-red-500' : 'text-green-700'
+                    } font-semibold text-lg`}
+                >
+                    {formMessage.message}
+                </p>
+            )}
             <button
                 type='submit'
                 className='my-6 py-4 w-full text-xl text-semibold text-white bg-[#275df5] hover:bg-[#275df5cc] rounded-[60px]'
