62aebdfa411d2466fcf0a846225dc02600429e56
nguyen-hai-nam <nam.nn265@gmail.com>
2023-11-06 17:01:47 +0700

    Feat: integrate employer authentication api

--- client/src/components/EmployerRegisterForm.tsx
+++ client/src/components/EmployerRegisterForm.tsx
@@ -1,8 +1,44 @@
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { employerRegister } from '~/services/api'
 
 const EmployerRegisterForm = () => {
+    const navigate = useNavigate()
+    const [formData, setFormData] = useState({
+        companyName: '',
+        email: '',
+        password: '',
+        passwordConfirm: ''
+    })
+
+    const [formMessage, setFormMessage] = useState({ type: '', message: '' })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        try {
+            const response = await employerRegister(formData)
+            if (response?.status === 200) {
+                setFormMessage({ type: 'success', message: 'Register successfully, redirecting to login ...' })
+                setTimeout(() => {
+                    navigate('../login')
+                }, 2000)
+            } else if (response?.status === 400) {
+                setFormMessage({ type: 'error', message: response.data.message })
+            } else {
+                setFormMessage({ type: 'error', message: 'Something went wrong' })
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
-        <form className='px-16 py-12 flex flex-col gap-6 bg-white rounded-lg'>
+        <form className='px-16 py-12 flex flex-col gap-6 bg-white rounded-lg' onSubmit={onSubmit}>
             <div className='mx-auto '>
                 <h1 className='font-serif text-4xl font-bold text-cyan-600'>ITJobHunter</h1>
                 <p className='text-center text-md font-semibold text-cyan-600'>Right place for employers!</p>
@@ -12,6 +48,21 @@ const EmployerRegisterForm = () => {
                 <span className='text-xl font-semibold text-[#275df5] hover:underline cursor-pointer'>
                     <Link to='../login'>Already have an account?</Link>
                 </span>
+            </div>
+            <div className='flex flex-col gap-2'>
+                <label htmlFor='companyName' className='font-semibold'>
+                    Company Name
+                </label>
+                <input
+                    type='text'
+                    id='companyName'
+                    name='companyName'
+                    required
+                    placeholder='Enter your company name'
+                    className='py-3 px-4 rounded-2xl border-black border focus:outline-[#275df5]'
+                    value={formData.companyName}
+                    onChange={handleChange}
+                />
             </div>
             <div className='flex flex-col gap-2'>
                 <label htmlFor='email' className='font-semibold'>
@@ -24,6 +75,8 @@ const EmployerRegisterForm = () => {
                     required
                     placeholder='Enter your email'
                     className='py-3 px-4 rounded-2xl border-black border focus:outline-[#275df5]'
+                    value={formData.email}
+                    onChange={handleChange}
                 />
             </div>
             <div className='flex flex-col gap-2'>
@@ -37,6 +90,8 @@ const EmployerRegisterForm = () => {
                     required
                     placeholder='Enter your password'
                     className='py-3 px-4 rounded-2xl border-black border focus:outline-[#275df5]'
+                    value={formData.password}
+                    onChange={handleChange}
                 />
             </div>
             <div className='flex flex-col gap-2'>
@@ -50,8 +105,19 @@ const EmployerRegisterForm = () => {
                     required
                     placeholder='Confirm your password'
                     className='py-3 px-4 rounded-2xl border-black border focus:outline-[#275df5]'
+                    value={formData.passwordConfirm}
+                    onChange={handleChange}
                 />
             </div>
+            {formMessage && (
+                <p
+                    className={`${
+                        formMessage.type === 'error' ? 'text-red-500' : 'text-green-700'
+                    } font-semibold text-lg`}
+                >
+                    {formMessage.message}
+                </p>
+            )}
             <button
                 type='submit'
                 className='my-6 py-4 w-full text-xl text-semibold text-white bg-[#275df5] hover:bg-[#275df5cc] rounded-[60px]'
