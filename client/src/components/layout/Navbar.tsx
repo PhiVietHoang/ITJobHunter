ab2df1d1ce3957a24ef2ea340469ee74ed0b688e
nguyen-hai-nam <nam.nn265@gmail.com>
2023-11-07 21:05:25 +0700

    Feat: navbar dynamic styling

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,7 +1,22 @@
+import { useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { RootState } from '~/store'
 import { Button } from '~/components/ui/button'
 import { Separator } from '~/components/ui/separator'
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar'
 
 const Navbar = () => {
+    const token = useSelector((state: RootState) => state.employeeAuth.token)
+    const employee = useSelector((state: RootState) => state.employeeAuth.employee)
+    const navigate = useNavigate()
+
+    const logout = () => {
+        localStorage.removeItem('token')
+        navigate('/')
+        window.location.reload()
+    }
+
     return (
         <nav className='py-4 flex justify-between items-center'>
             <div className='flex items-center gap-12'>
@@ -14,19 +29,49 @@ const Navbar = () => {
                 </Button>
             </div>
             <div className='flex items-center gap-4'>
-                <Button
-                    variant='outline'
-                    className='px-6 text-md rounded-full border-[#275df5] text-[#275df5] hover:text-[#275df5]'
-                >
-                    Login
-                </Button>
-                <Button variant='default' className='px-6 text-md rounded-full bg-[#f05537] hover:bg-[#f05537dd]'>
-                    Register
-                </Button>
-                <Separator orientation='vertical' className='w-2' />
-                <Button variant='ghost' className='px-6 text-md text-gray-500 rounded-full'>
-                    For employers
-                </Button>
+                {!token && (
+                    <Link to='auth/login'>
+                        <Button
+                            variant='outline'
+                            className='px-6 text-md rounded-full border-[#275df5] text-[#275df5] hover:text-[#275df5]'
+                        >
+                            Login
+                        </Button>
+                    </Link>
+                )}
+                {!token && (
+                    <Link to='auth/register'>
+                        <Button
+                            variant='default'
+                            className='px-6 text-md rounded-full bg-[#f05537] hover:bg-[#f05537dd]'
+                        >
+                            Register
+                        </Button>
+                    </Link>
+                )}
+                {token && (
+                    <Avatar>
+                        <AvatarImage src={employee?.avatar} alt='Profile Picture' />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                )}
+                {token && (
+                    <Button
+                        variant='outline'
+                        className='px-6 text-md rounded-full border-[#275df5] text-[#275df5] hover:text-[#275df5]'
+                        onClick={logout}
+                    >
+                        Logout
+                    </Button>
+                )}
+                {!token && <Separator orientation='vertical' className='w-2' />}
+                {!token && (
+                    <Link to='employer/auth/login'>
+                        <Button variant='ghost' className='px-6 text-md text-gray-500 rounded-full'>
+                            For employers
+                        </Button>
+                    </Link>
+                )}
             </div>
         </nav>
     )
