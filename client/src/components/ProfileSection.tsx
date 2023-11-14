23ad4c8acea40d1f55027c75e51fdb33e1be8f78
nguyen-hai-nam <nam.nn265@gmail.com>
2023-11-15 00:12:20 +0700

    Feat: add children to profile section component

--- client/src/components/ProfileSection.tsx
+++ client/src/components/ProfileSection.tsx
@@ -1,9 +1,20 @@
-const ProfileSection = (props: { title: string; description: string }) => {
+import { ReactNode } from 'react'
+
+interface ProfileSectionProps {
+    title: string
+    description: string
+    children?: ReactNode
+}
+
+const ProfileSection = ({ title, description, children }: ProfileSectionProps) => {
     return (
         <div className='relative w-full p-8 bg-white rounded-3xl shadow-sm'>
             <button className='absolute right-8 text-[#275df5] font-semibold hover:underline'>Edit</button>
-            <h3 className='font-semibold'>{props.title}</h3>
-            <p className='mt-4 text-sm text-gray-600'>{props.description}</p>
+            <h3 className='font-semibold'>{title}</h3>
+            <div className='mt-4'>
+                {!children && <p className='text-sm text-gray-600'>{description}</p>}
+                {children}
+            </div>
         </div>
     )
 }
